a062030291a12bd582eca99e6bf46f487f6afa9d
Your Name <you@example.com>
2021-01-08 15:53:07 -0800

    Completed part 1.

--- index.js
+++ index.js
@@ -2,5 +2,16 @@ const express = require('express')
 const app = express() // Creating an instance of express.
 
 app.get('/', (request, response) => {
-    response.send('Response here')
+    response.send('This is the Home Page!')
 })
+
+app.get('/about', (request, response) => {
+    response.send("Some stuff about me will go here.")
+})
+
+app.get('/blog', (request, response) => {
+    response.send("A directory of all my blog posts will go here.")
+})
+
+// http://localhost:3000/
+app.listen(3000, () => console.log("Listening on port 3000!"))
