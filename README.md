0e498a98a61e940169a2dfb5b9e2801fd7e116ef
Your Name <you@example.com>
2021-01-08 16:22:58 -0800

    Updated response to send a file of an html file we coded, instead of just hardcoded text.

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 Doing the exercise from below link:
 https://gawdiseattle.gitbook.io/wdi/05-node-express/00readme-1/03views
+
+1. mkdir express-personal-website.
+2. touch index.js
+2. npm init -y // This adds node to our folder. We can now use `node index.js` to run our file in our JS file in our terminal.
+3. npm install express // This creates a package-lock.json file. Want to download it locally and not globally.
+4. In the index.js file, add:
+    const express = require('express') // Import express, where the name of the constant is up to you.
+    const app = express() // Create an instance of an express app, where the name of the constant is up to you.
+5. If you don't have nodemon (automatically runs `node index.js` whenever you save your file), download it globally: `npm install nodemon -g`
--- index.js
+++ index.js
@@ -1,16 +1,22 @@
-const express = require('express')
-const app = express() // Creating an instance of express.
+const express = require('express') // Import express.
+const app = express() // Create an instance of express.
 
+// When we navigate to http://localhost:3000/, then the thing we send when the response called 'response' sends 'This is the Home Page!'
 app.get('/', (request, response) => {
-    response.send('This is the Home Page!')
+    // response.send('This is the Home Page!')
+    // response.sendFile takes an aboslute path
+    // __dirname is a built-in node variable we can use.
+    response.sendFile(__dirname+'/views/index.html') 
 })
 
 app.get('/about', (request, response) => {
-    response.send("Some stuff about me will go here.")
+    // response.send("Some stuff about me will go here.")
+    response.sendFile(__dirname+'/views/about.html')
 })
 
 app.get('/blog', (request, response) => {
-    response.send("A directory of all my blog posts will go here.")
+    // response.send("A directory of all my blog posts will go here.")
+    response.sendFile(__dirname+'/views/blog-directory.html')
 })
 
 // http://localhost:3000/
